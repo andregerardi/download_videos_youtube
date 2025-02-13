29eabdebed9248e9669c8f0a5fdae70928cade5c
Dirceu André Gerardi <48441841+andregerardi@users.noreply.github.com>
2025-02-13 13:24:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,32 @@ def baixar_video(url):
     # Baixar o vídeo
     ys = yt.streams.get_highest_resolution()
     
-    return ys, text
+    # baixar
+    video_filename = "video.mp4"
+    ys.download(filename=video_filename)
+    st.success("Vídeo gerado com sucesso!")
+    
+    # Salvar as legendas em um arquivo
+    legenda_filename = "legendas.txt"
+    with open(legenda_filename, "w", encoding="utf-8") as f:
+        f.write(text)
+
+    st.success("Legendas geradas com sucesso!")
+    
+    # Criar um arquivo zip contendo o vídeo e as legendas
+    zip_filename = "video_e_legendas.zip"
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        zipf.write(video_filename)
+        zipf.write(legenda_filename)
+
+    # Exibir o botão para baixar o arquivo zip
+    with open(zip_filename, "rb") as f:
+        st.download_button(
+            label="Baixar Vídeo e Legendas",
+            data=f,
+            file_name=zip_filename,
+            mime="application/zip"
+        )
 
 # Interface Streamlit
 st.title("🤖 Download de Vídeos do YouTube")
@@ -39,38 +64,6 @@ url = st.text_input("Insira a URL do vídeo do YouTube:")
 
 if st.button("Processar"):
     if url:
-        ys, text = baixar_video(url)
-
-        # baixar
-        video_filename = "video.mp4"
-        ys.download(filename=video_filename)
-        st.success("Vídeo gerado com sucesso!")
-        
-        # Salvar as legendas em um arquivo
-        legenda_filename = "legendas.txt"
-        with open(legenda_filename, "w", encoding="utf-8") as f:
-            f.write(text)
-
-        st.success("Legendas geradas com sucesso!")
-        
-        # Criar um arquivo zip contendo o vídeo e as legendas
-        zip_filename = "video_e_legendas.zip"
-        with zipfile.ZipFile(zip_filename, 'w') as zipf:
-            zipf.write(video_filename)
-            zipf.write(legenda_filename)
-
-        # Exibir o botão para baixar o arquivo zip
-        with open(zip_filename, "rb") as f:
-            st.download_button(
-                label="Baixar Vídeo e Legendas",
-                data=f,
-                file_name=zip_filename,
-                mime="application/zip"
-            )
-
-        # Limpar os arquivos temporários
-        os.remove(video_filename)
-        os.remove(legenda_filename)
-        os.remove(zip_filename)
+        baixar_video(url)
     else:
         st.error("Por favor, insira uma URL válida.")
